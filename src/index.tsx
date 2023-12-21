fef394b79cabb03b4c341dc0320a1582e7a8f410
saltire sable <saltiresable@gmail.com>
2023-12-21 18:00:15 -0500

    Add redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
+import { store } from './store';
 
 
 const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
-  root.render(<App name='React TypeScript App' />);
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
 }
